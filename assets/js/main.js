0b223340aa4bc0008f77c407b852d873e9188458
riaz57 <riaztest57@gmail.com>
2021-10-14 09:17:07 +0600

    added new section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,49 @@
 
 $(function(){
 
+$('.categories').slick({
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  autoplay: true,
+  loop: true,
+  swipe: 1500,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 330,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ],
+  autoplaySpeed: 1500,
+  pauseOnHover: true,
+  pauseOnFocus: true
+});
+
+
 $('#best_seller .product_slider').owlCarousel({
 	// autoplay:true,
 	loop:true,
